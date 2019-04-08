e28cbd4b92b8604551e2ec38dae6bcea5019e0a1
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-04-08 10:05:07 +0200

    La till en ny boktitel + la till lite ny kod

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
+let bookList = [
+	{"title": "Hunger", "author": "Roxane Gay", "pages": 320},
+	{"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
+	{"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+	{"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
+]
+
 const Book = ({title, author, pages}) => {
-    return (
-        <section>
-            <h2>{title}</h2>
-            <p>by: {author}</p>
-            <p>Pages: {pages} pages</p>
-        </section>
-    )
+	return (
+		<section>
+			<h2>{title}</h2>
+			<p>by: {author}</p>
+			<p>Pages: {pages} pages</p>
+		</section>
+	)
 }
 
-const Library = () => {
-    return (
-        <div>
-            <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-            <Book title="White Teeth" author="Zadie Smith" pages={480}/>
-            <Book title="Cat´s Cradle" author="Kurt Vonnegut" pages={304}/>
-        </div>
-    )
+const Library = ({books}) => {
+	return (
+		<div>
+			{books.map(
+				(book, i) => 
+					<Book 
+						key={i}
+						title={book.title} 
+						author={book.author} 
+						pages={book.pages}/>
+			)}
+		</div>
+	)
 }
+
+
 
 render(
-    <Library />, 
-    document.getElementById('root')
+	<Library books={bookList}/>, 
+	document.getElementById('root')
 )
